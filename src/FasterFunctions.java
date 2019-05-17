369d1be04d7217b8577263d19831f4ef5ee7feba
League Student <league@iMac-28.attlocal.net>
2019-05-16 17:49:22 -0700

    Done

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -5,7 +5,22 @@ public class FasterFunctions {
 		long startTime = System.currentTimeMillis();
 		//Your Code Here
 		
+		Thread thread1 = new Thread(()->SlowFunctions.slowSortLargeArray());
+		Thread thread2 = new Thread(()->SlowFunctions.ackermann(3,14));
+		Thread thread3 = new Thread(()->SlowFunctions.millionsOfSqrts());
 		
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		
+		try {
+			thread1.join();
+			thread2.join();
+			thread3.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
